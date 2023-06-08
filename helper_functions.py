680960831d51062d4e2d410032a64caa29a19c31
baruch-susman <baruch.susman@mail.huji.ac.il>
2023-06-08 21:56:06 +0300

    finished 2 and 3 generally. weird charge distributions. check units and proper sums of field vector

--- helper_functions.py
+++ helper_functions.py
@@ -41,6 +41,11 @@ def polar_to_cartesian(r, theta):
     z = 0
     return x, y, z
 
+def cartesian_to_polar(x, y):
+    r = math.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return r, theta
+
 
 def get_random_velocity(v: float, dim: int):
     if dim == 2:
